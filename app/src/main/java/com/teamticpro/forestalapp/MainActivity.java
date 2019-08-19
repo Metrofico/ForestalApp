8bad3416ad487e79d588a1c10f11ccde41050f1c
Metrofico <joy_12lino@outlook.com>
2019-08-19 03:41:36 -0500

    Objeto Mensajes Agregado!,  - Controla Mensajes. desde Message.java  - Obtiene Mensaje Aleatorio desde Message.java

--- app/src/main/java/com/teamticpro/forestalapp/MainActivity.java
+++ app/src/main/java/com/teamticpro/forestalapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.teamticpro.forestalapp;
 
 import android.os.Bundle;
-import android.util.SparseArray;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,15 +9,31 @@ import com.teamticpro.forestalapp.Objetos.Message;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SparseArray<Message> mensajes = new SparseArray<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onLoadMessages();
+        setupMessages();
     }
 
     public void onLoadMessages(){
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 1","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 2","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 3","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 4","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 5","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+        Message.addMessage("CUIDA EL MENDIO AMBIENTE 6","Asdjkahsjdhasjkdhjkashdjkashdjkhasjkdhasjkhdjka");
+    }
 
+    public void setupMessages(){
+        TextView titulo_view = findViewById(R.id._texto_aleatorio_titulo);
+        TextView message_view = findViewById(R.id._texto_aleatorio);
+        Message m = Message.getRandomMessage();
+        titulo_view.setText(m.getTitle());
+        message_view.setText(m.getMessage());
     }
 }
--- app/src/main/java/com/teamticpro/forestalapp/Objetos/Message.java
+++ app/src/main/java/com/teamticpro/forestalapp/Objetos/Message.java
@@ -1,6 +1,24 @@
 package com.teamticpro.forestalapp.Objetos;
 
+import android.util.SparseArray;
+
+import java.util.Random;
+
 public class Message {
+
+    private static final SparseArray<Message> mensajes = new SparseArray<>();
+
+    public static void addMessage(String title, String message){
+        mensajes.put(mensajes.size(), new Message(title, message));
+    }
+    public static Message getRandomMessage(){
+        Random random = new Random();
+        int _INDEX_ = random.nextInt(mensajes.size());
+        return mensajes.get(_INDEX_);
+    }
+
+
+
 
     private String title;
     private String message;
